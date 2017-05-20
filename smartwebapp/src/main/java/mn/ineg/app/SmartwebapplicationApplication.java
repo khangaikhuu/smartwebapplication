d1c252f8aa0f47a7f75e31b2a2e30d22f4b459c2
khangaikhuu <khangaikhuu@googlemail.com>
2017-05-20 19:13:36 +0800

    initial ineg web application with wicket framework

---
+++ smartwebapp/src/main/java/mn/ineg/app/SmartwebapplicationApplication.java
@@ -0,0 +1,12 @@
+package mn.ineg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwebapplicationApplication.class, args);
+	}
+}
